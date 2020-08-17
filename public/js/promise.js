e8611db6c587c58d17920ef39c93adbff4b5e234
LsJoker <30564101+LsJoker@users.noreply.github.com>
2020-08-17 22:54:19 +0800

    reject

--- public/js/promise.js
+++ public/js/promise.js
@@ -10,11 +10,45 @@ function Promise (fn){
 
 	function resolve (newVal) {
 		//fulllfilled状态返回
-		if (state !== "pending") return;
-		state = "fullfilled";
-		val = newVal;
-		//下一个回调执行
-		handleCb(newVal);
+		const fn = ()=>{
+			if (state !== "pending") return;
+			//问题1：如果then中执行一个函数返回的是promise。则返回不正确
+			if(newVal && (typeof newVal === "object" || typeof newVal === "function")) {
+				const {then} = newValue
+				if(typeof then === 'function'){   
+					// newValue 为新产生的 Promise,此时resolve为上个 promise 的resolve
+					//相当于调用了新产生 Promise 的then方法，注入了上个 promise 的resolve 为其回调
+					then.call(newValue,resolve)
+					return
+				}
+			}
+			state = "fullfilled";
+			val = newVal;
+			//下一个回调执行
+			handleCb(newVal);
+		}
+		setTimeout(fn,0);
+	}
+	function reject (error) {
+		//fulllfilled状态返回
+		const fn = ()=>{
+			if (state !== "pending") return;
+			//问题1：如果then中执行一个函数返回的是promise。则返回不正确
+			if(error && (typeof error === "object" || typeof error === "function")) {
+				const {then} = error
+				if(typeof then === 'function'){   
+					// newValue 为新产生的 Promise,此时resolve为上个 promise 的resolve
+					//相当于调用了新产生 Promise 的then方法，注入了上个 promise 的resolve 为其回调
+					then.call(error,resolve)
+					return
+				}
+			}
+			state = "rejected";
+			val = error;
+			//下一个回调执行
+			handleCb(newVal);
+		}
+		setTimeout(fn,0);
 	}
 	function handleCb (newVal) {
 		while(cbs.length>1) {
@@ -27,17 +61,30 @@ function Promise (fn){
 			cbs.push(callback);
 			return;
 		}
-		if (state === "fullfilled") {
-			//处理回调
-			const res = callback.onfullfilled(val);
-			//处理下一个promise
-			callback.resolve(res);
+
+		const cb = state === "fullfilled" ? callback.onfullfilled : callback.onrejected;
+		const next = state === "fullfilled" ? callback.resolve : callback.reject;
+		if (!cb) {
+			next(val);
+			return;
 		}
+		const res = cb(val);
+		next(val);
+		// if (state === "fullfilled") {
+		// 	if(!callback.onFulfilled){
+		// 		callback.resolve(value)
+		// 		return; 
+		// 	}
+		// 	//处理回调
+		// 	const res = callback.onfullfilled(val);
+		// 	//处理下一个promise
+		// 	callback.resolve(res);
+		// }
 	}
 	//then方法
-	this.then = (onfullfilled)=>{
+	this.then = (onfullfilled,onrejected)=>{
 		return new Promise((resolve,reject)=>{
-			handleCb({resolve,onfullfilled})
+			handleCb({resolve,onfullfilled,onrejected,reject})
 		})
 	} 
 }
